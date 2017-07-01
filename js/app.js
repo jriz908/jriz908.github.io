58bba055e985a5ac3ac6fff1b65fd6b2066ba3f2
Jacob Rizer <jriz908@gmail.com>
2017-07-01 16:47:18 -0400

    ...

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ app.config(function($routeProvider, $locationProvider){
   $routeProvider.when('/',{
   	controller: 'home-controller',
     templateUrl: 'views/home.html'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
   $locationProvider.html5Mode(true);
 });
